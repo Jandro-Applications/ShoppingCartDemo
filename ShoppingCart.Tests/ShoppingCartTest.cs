b9546f489f662a4bfb1ffcba6820190b5ac67fe0
Jandro Applications <japplications@outlook.com>
2020-07-27 15:13:59 +0200

    Inicijalni projekti i namespaceovi

---
+++ ShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class ShoppingCartTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
